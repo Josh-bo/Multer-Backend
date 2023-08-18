2b678831adceb5f0bf50d0def6c6dd0fbf9d96a4
Bamidele joshua olayinka <Olayinka775@gmail.com>
2023-08-18 13:53:17 +0100

    First commit here

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const mongoose = require('mongoose');
+const cors = require('cors');
+const multer = require('multer');
+const path = require('path');
+const userModel = require('./models/user.model');
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'))
+
+mongoose.connect('mongodb+srv://JOSHBAM:OLAYINKA1@cluster0.whles85.mongodb.net/Zazuu?retryWrites=true&w=majority')
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/Images')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + "_" + Date.now() + path.extname(file.originalname))
+    }
+})
+
+const upload = multer({
+    storage: storage
+})
+
+app.post('/upload', upload.single('file'), (req, res) => {
+    userModel.create({image: req.file.filename})
+    .then(result => res.json(result))
+    .catch(err => console.log(err))
+})
+
+app.get('/getImage', (req, res) => {
+    userModel.find()
+    .then(users => res.json(users))
+    .catch(err => console.log(err))
+})
+
+app.listen(3001, () => {
+    console.log("Server Is Running at port 3001");
+})

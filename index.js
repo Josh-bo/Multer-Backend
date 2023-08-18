6196abdcc02a71b1fc9d088cf3d27320a3d2fc9b
Bamidele joshua olayinka <Olayinka775@gmail.com>
2023-08-18 14:10:25 +0100

    Looks good

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require("express");
+require('dotenv').config()
+const PORT = process.env.PORT || 5001
 const mongoose = require('mongoose');
+let URI = process.env.MONGO_URI
+mongoose.connect(URI)
+.then(()=>console.log("MongoDB connected"))
+.catch((err)=>console.log(err))
+// const mongoose = require('mongoose');
 const cors = require('cors');
 const multer = require('multer');
 const path = require('path');
@@ -9,7 +16,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'))
 
-mongoose.connect('mongodb+srv://JOSHBAM:OLAYINKA1@cluster0.whles85.mongodb.net/Zazuu?retryWrites=true&w=majority')
+// mongoose.connect('mongodb+srv://JOSHBAM:OLAYINKA1@cluster0.whles85.mongodb.net/Zazuu?retryWrites=true&w=majority')
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -36,6 +43,6 @@ app.get('/getImage', (req, res) => {
     .catch(err => console.log(err))
 })
 
-app.listen(3001, () => {
+app.listen(PORT, () => {
     console.log("Server Is Running at port 3001");
 })
